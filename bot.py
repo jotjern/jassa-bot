62a52e29b600d49687121b21408e8e84033770f5
Jorgen1040 <jorgen.mogle@gmail.com>
2020-09-27 15:42:21 +0200

    Added check for already made gifs

--- bot.py
+++ bot.py
@@ -28,24 +28,31 @@ async def jasså(ctx, args):
     #if args == discord.User: 
     #    name = ctx.
     
+    
+
     name = hashlib.md5(args.encode()).hexdigest()
     filename = "output/"+name+".mp4"
     optimized ="output/optimized/"+name+".gif"
-    
-    video = VideoFileClip(os.path.abspath("tmp/jassa_template.mp4")).subclip(0,3)
 
-    txt_clip = ( TextClip(args,fontsize=32,color='white',font='ProximaNova-Semibold.otf')
-                 .set_position((160,655))
-                 .set_duration(3) )
+    if os.path.isfile(optimized): 
+        print("Gif allerede lagd, sender fil")
+        await ctx.send(file=discord.File(optimized))
+    else:
+        await ctx.send("Lager ny gif nå :)")
+        video = VideoFileClip(os.path.abspath("tmp/jassa_template.mp4")).subclip(0,3)
 
-    result = CompositeVideoClip([video, txt_clip]) 
-    #result.write_gif(filename,fps=25, program='ffmpeg')
-    result.write_videofile(filename)
-    # New better ffmpeg options
-    os.system("ffmpeg -y -i "+filename+" -i tmp/palette.png -lavfi 'fps=19,scale=480:-1:flags=lanczos,paletteuse=dither=bayer:bayer_scale=5:diff_mode=rectangle' "+optimized)
-    
-    await ctx.send(file=discord.File(optimized))
-    print("Successfully generated gif with "+args)
+        txt_clip = ( TextClip(args,fontsize=33,color='white',font='ProximaNova-Semibold.otf')
+                    .set_position((160,655))
+                    .set_duration(3) )
+
+        result = CompositeVideoClip([video, txt_clip]) 
+        #result.write_gif(filename,fps=25, program='ffmpeg')
+        result.write_videofile(filename)
+        # New better ffmpeg options
+        os.system("ffmpeg -y -i "+filename+" -i tmp/palette.png -lavfi 'fps=19,scale=480:-1:flags=lanczos,paletteuse=dither=bayer:bayer_scale=5:diff_mode=rectangle' "+optimized)
+        
+        await ctx.send(file=discord.File(optimized))
+        print("Successfully generated gif with "+args)
 
 @bot.command()
 async def close(ctx):
