e430e80102ccbb0a1bdc3da5fa9f08aadfd40fce
Jorgen1040 <jorgen.mogle@gmail.com>
2020-09-30 15:46:00 +0200

    Removed logger since it is unused

--- src/bot.py
+++ src/bot.py
@@ -6,11 +6,11 @@ from discord.ext import commands
 import hashlib
 import rule34
 
-logger = logging.getLogger('discord')
-logger.setLevel(logging.INFO)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
+# logger = logging.getLogger('discord')
+# logger.setLevel(logging.INFO)
+# handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+# handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+# logger.addHandler(handler)
 
 rule34 = rule34.Sync()
 
